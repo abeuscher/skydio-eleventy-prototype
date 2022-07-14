35c17991be9931af25bc3960037521c45acaca18
Alex Beuscher <alexbeuscher@gmail.com>
2022-07-14 00:17:53 -0700

    Added image support

--- web/.eleventy.js
+++ web/.eleventy.js
@@ -1,8 +1,17 @@
 const util = require("util");
+const pluginSass = require("eleventy-plugin-sass");
+const urlFor = require("./src/utils/imageUrl");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("debug", function (value) {
     return util.inspect(value, { compact: false });
+  });
+  eleventyConfig.addPlugin(pluginSass);
+  eleventyConfig.addShortcode("imageUrlFor", (image, width = "400") => {
+    return urlFor(image).width(width).auto("format");
+  });
+  eleventyConfig.addShortcode("croppedUrlFor", (image, width, height) => {
+    return urlFor(image).width(width).height(height).auto("format");
   });
   return {
     dir: {
---
+++ web/src/_data/homepage.js
@@ -0,0 +1,17 @@
+const groq = require("groq");
+const client = require("../utils/sanityClient");
+module.exports = async function () {
+  const homeid = await client.fetch(groq`
+    *[_type == "siteConfig"]{
+      ...,
+    }[0].frontpage._ref
+  `);
+  return await client.fetch(
+    groq`
+*[_type == "landingPage" && _id==$homeid]{
+      ...,
+    }[0]
+  `,
+    { homeid: homeid }
+  );
+};
